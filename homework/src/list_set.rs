06543698d91d95c7f0e1687d1e7200199de88465
Lee Jong Heon <squareone@kaist.ac.kr>
2022-10-08 18:27:58 +0000

    hw3:fix_use_after_free

--- homework/src/list_set.rs
+++ homework/src/list_set.rs
@@ -36,7 +36,22 @@ impl<'l, T: Ord> Cursor<'l, T> {
     /// Move the cursor to the position of key in the sorted list. If the key is found in the list,
     /// return `true`.
     fn find(&mut self, key: &T) -> bool {
-        todo!()
+        //todo!()
+
+        unsafe {
+            while !(*(self.0)).is_null() {
+                let node_key = &(**(self.0)).data;
+                if node_key.cmp(key) == std::cmp::Ordering::Equal {
+                    return true;
+                }
+
+                let new_cursor = Cursor((**(self.0)).next.lock().unwrap());
+                let old_cursor = std::mem::replace(self, new_cursor);
+
+                drop(old_cursor.0);
+            }
+            false
+        }
     }
 }
 
@@ -51,22 +66,114 @@ impl<T> OrderedListSet<T> {
 
 impl<T: Ord> OrderedListSet<T> {
     fn find(&self, key: &T) -> (bool, Cursor<T>) {
-        todo!()
+        //todo!()
+        let mut cursor = Cursor(self.head.lock().unwrap());
+        let result = cursor.find(key);
+        (result, cursor)
     }
 
     /// Returns `true` if the set contains the key.
     pub fn contains(&self, key: &T) -> bool {
-        todo!()
+        let mut cursor = Cursor(self.head.lock().unwrap());
+        let result = cursor.find(key);
+        drop(cursor.0);
+        result
     }
 
     /// Insert a key to the set. If the set already has the key, return the provided key in `Err`.
     pub fn insert(&self, key: T) -> Result<(), T> {
-        todo!()
+        let mut head_node_guard = self.head.lock().unwrap();
+
+        unsafe {
+            // Case1: list is empty -> insert at first
+            if (*head_node_guard).is_null() {
+                let new_node = Node::new(key, ptr::null_mut());
+                *head_node_guard = new_node;
+                return Ok(());
+            }
+
+            // Case2: given key is smaller than the key of head
+            let first_key = &(**head_node_guard).data;
+            if first_key.cmp(&key) == std::cmp::Ordering::Greater {
+                let new_node = Node::new(key, *head_node_guard);
+                *head_node_guard = new_node;
+                return Ok(());
+            }
+
+            while !(*head_node_guard).is_null() {
+                let first_key = &(**head_node_guard).data;
+                let mut next_node_guard = (**head_node_guard).next.lock().unwrap();
+
+                // find a duplicate key
+                if key.cmp(first_key) == std::cmp::Ordering::Equal {
+                    return Err(key);
+                }
+
+                // reaching last node of the list
+                if (*next_node_guard).is_null() {
+                    *next_node_guard = Node::new(key, ptr::null_mut());
+                    return Ok(());
+                }
+
+                // find an appropriate place
+                let next_key = &(**next_node_guard).data;
+                if key.cmp(next_key) == std::cmp::Ordering::Less {
+                    *next_node_guard = Node::new(key, *next_node_guard);
+                    return Ok(());
+                }
+
+                // move to next node
+                drop(head_node_guard);
+                head_node_guard = next_node_guard;
+            }
+            Err(key)
+        }
     }
 
     /// Remove the key from the set and return it.
     pub fn remove(&self, key: &T) -> Result<T, ()> {
-        todo!()
+        let mut head_node_guard = self.head.lock().unwrap();
+
+        unsafe {
+            // list is empty
+            if (*head_node_guard).is_null() {
+                return Err(());
+            }
+
+            // remove the first node
+            if (**head_node_guard).data.cmp(key) == std::cmp::Ordering::Equal {
+                let second_node_guard = (**head_node_guard).next.lock().unwrap();
+                let box_node = Box::from_raw(*head_node_guard);
+                *head_node_guard = *second_node_guard;
+                // free first node
+
+                return Ok((*box_node).data);
+            }
+
+            while !(*head_node_guard).is_null() {
+                let mut next_node_guard = (**head_node_guard).next.lock().unwrap();
+
+                // If second node is empty
+                if (*next_node_guard).is_null() {
+                    return Err(());
+                }
+
+                let mut next_node_data = &(**next_node_guard).data;
+
+                // compare key with that of second node
+                if next_node_data.cmp(key) == std::cmp::Ordering::Equal {
+                    let box_node = Box::from_raw(*next_node_guard);
+                    let third_guard = (**next_node_guard).next.lock().unwrap();
+                    *next_node_guard = *third_guard;
+
+                    return Ok((*box_node).data);
+                }
+
+                drop(head_node_guard);
+                head_node_guard = next_node_guard;
+            }
+            Err(())
+        }
     }
 }
 
@@ -84,13 +191,44 @@ impl<'l, T> Iterator for Iter<'l, T> {
     type Item = &'l T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        todo!()
+        //todo!()
+        unsafe {
+            let guard = self.0.take().unwrap();
+
+            if (*guard).is_null() {
+                return None;
+            }
+
+            let data = &(**guard).data;
+            let next_guard = (**guard).next.lock().unwrap();
+            drop(guard);
+
+            self.0 = Some(next_guard);
+            Some(data)
+        }
     }
 }
 
 impl<T> Drop for OrderedListSet<T> {
     fn drop(&mut self) {
-        todo!()
+        let mut node_ptr = *self.head.get_mut().unwrap();
+        unsafe {
+            // list is empty
+            if (node_ptr).is_null() {
+                return;
+            }
+
+            let mut next_node_ptr = *(*node_ptr).next.get_mut().unwrap();
+            let node_to_delete = Box::from_raw(node_ptr);
+            drop(node_to_delete);
+
+            while !(next_node_ptr).is_null() {
+                node_ptr = next_node_ptr;
+                next_node_ptr = *(*node_ptr).next.lock().unwrap();
+                let node_to_delete = Box::from_raw(node_ptr);
+                drop(node_to_delete);
+            }
+        }
     }
 }
 
